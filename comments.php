5e154e7f553689ebe16f42f472ae351278f14bba
Andrew Misplon <amisplon@gmail.com>
2017-07-12 09:37:28 +0200

    Comment style improvements

--- comments.php
+++ comments.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying comments
+ * The template for displaying comments.
  *
  * This is the template that displays the area of the page that contains both the current comments
  * and the comment form.
@@ -8,7 +8,7 @@
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package siteorigin-corp
- * @license GPL 2.0 
+ * @license GPL 2.0
  */
 
 /*
@@ -24,26 +24,23 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 
 	<?php
-	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
 				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'siteorigin-corp' ) ),
+					esc_html( _nx( '1 Comment', '%1$s Comments', get_comments_number(), 'comments title', 'siteorigin-corp' ) ),
 					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			?>
-		</h2><!-- .comments-title -->
+		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'siteorigin-corp' ); ?></h2>
 			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'siteorigin-corp' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'siteorigin-corp' ) ); ?></div>
-
+				<div class="nav-previous"><?php previous_comments_link( '<span class="icon-long-arrow-left"></span> ' . esc_html__( 'Older comments', 'siteorigin-corp' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer comments', 'siteorigin-corp' ) . ' <span class="icon-long-arrow-right"></span>' ); ?></div>
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
@@ -51,7 +48,7 @@ if ( post_password_required() ) {
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
-					'style'      => 'ol',
+					'style'	=> 'ol',
 					'callback' => 'siteorigin_corp_comment',
 					'short_ping' => true,
 				) );
@@ -62,10 +59,8 @@ if ( post_password_required() ) {
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'siteorigin-corp' ); ?></h2>
 			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'siteorigin-corp' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'siteorigin-corp' ) ); ?></div>
-
+				<div class="nav-previous"><?php previous_comments_link( '<span class="icon-long-arrow-left"></span> ' . esc_html__( 'Older comments', 'siteorigin-corp' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer comments', 'siteorigin-corp' ) . ' <span class="icon-long-arrow-right"></span>' ); ?></div>
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
 		<?php
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -61,14 +61,13 @@ function siteorigin_corp_comment( $comment, $args, $depth ) {
 		<div class="comment-box">
 			<?php if ( $type == 'comment' ) : ?>
 				<div class="avatar-container">
-					<?php echo get_avatar( get_comment_author_email(), 70 ) ?>
+					<?php echo get_avatar( get_comment_author_email(), 60 ) ?>
 				</div>
 			<?php endif; ?>
 
 			<div class="comment-container">
 				<div class="info">
-					<span class="author"><?php comment_author_link() ?></span><br>
-					<span class="date"><?php comment_date() ?></span>
+					<span class="author"><?php comment_author_link(); ?></span><span class="date"><?php comment_date(); ?></span>
 				</div>
 
 				<div class="comment-content content">
@@ -377,3 +376,39 @@ function siteorigin_corp_category_transient_flusher() {
 }
 add_action( 'edit_category', 'siteorigin_corp_category_transient_flusher' );
 add_action( 'save_post',     'siteorigin_corp_category_transient_flusher' );
+
+if ( ! function_exists( 'siteorigin_corp_comment' ) ) :
+/**
+ * The callback function for wp_list_comments in comments.php.
+ *
+ * @link https://codex.wordpress.org/Function_Reference/wp_list_comments.
+ */		
+function siteorigin_corp_comment( $comment, $args, $depth ) {
+	?>
+	<li <?php comment_class() ?> id="comment-<?php comment_ID() ?>">
+		<?php $type = get_comment_type( $comment->comment_ID ); ?>
+		<div class="comment-box">
+			<?php if ( $type == 'comment' ) : ?>
+				<div class="avatar-container">
+					<?php echo get_avatar( get_comment_author_email(), 60 ) ?>
+				</div>
+			<?php endif; ?>
+
+			<div class="comment-container">
+				<div class="info">
+					<span class="author"><?php comment_author_link(); ?></span><br>
+					<span class="date"><?php comment_date( apply_filters( 'siteorigin_corp_date_format', 'F d, Y' ) ); ?></span>
+				</div>
+
+				<div class="comment-content content">
+					<?php comment_text(); ?>
+				</div>
+
+				<?php if ( $depth <= $args['max_depth'] ) : ?>
+					<?php comment_reply_link( array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ?>
+				<?php endif; ?>
+			</div>
+		</div>
+	<?php
+}
+endif;
