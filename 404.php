1d62d38b43f7cd137391ec5aa6c269332e893f59
Andrew Misplon <amisplon@gmail.com>
2017-07-13 20:19:17 +0200

    Remove sidebar from 404 page

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * The template for displaying 404 pages (not found)
+ * The template for displaying 404 pages (not found).
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
  * @package siteorigin-corp
- * @license GPL 2.0
+ * @license GPL 2.0 
  */
 
 get_header(); ?>
@@ -28,5 +28,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
