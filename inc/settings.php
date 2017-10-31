ed03ed47cc4f0db03e734a7be2d0d0be48feb1cd
Andrew Misplon <amisplon@gmail.com>
2017-10-31 12:21:34 +0200

    Add the blog settings
    Remove the sticky class

--- inc/settings.php
+++ inc/settings.php
@@ -48,44 +48,38 @@ function siteorigin_corp_settings_init() {
 					'label' => esc_html__( 'Sticky Header Scales Logo', 'siteorigin-corp' ),
 					'description' => esc_html__( 'Scales the logo down as the header becomes sticky.', 'siteorigin-corp' )
 				),						
-			)
+			),
 		),
 
 		// Navigation.
 		'navigation' => array(
 			'title' => esc_html__( 'Navigation', 'siteorigin-corp' ),
 			'fields' => array(
-
 				'header_menu' => array(
 					'type' => 'checkbox',
 					'label' => esc_html__( 'Header Menu', 'siteorigin-corp' ),
 					'description' => esc_html__( 'Display the main menu in the header.', 'siteorigin-corp' )
 				),
-
 				'mobile_menu' => array(
 					'type' => 'checkbox',
 					'label' => esc_html__( 'Mobile Menu', 'siteorigin-corp' ),
 					'description' => esc_html__( 'Use a mobile menu for small screen devices. Header Menu setting must be enabled.', 'siteorigin-corp' )
 				),	
-
 				'mobile_menu_collapse' => array(
 					'label' => esc_html__( 'Mobile Menu Collapse', 'siteorigin-corp' ),
 					'type' => 'number',
 					'description' => esc_html__( 'The pixel resolution when the primary menu collapses into the mobile menu.', 'siteorigin-corp' )
 				),							
-
 				'menu_search' => array(
 					'type' => 'checkbox',
 					'label' => esc_html__( 'Menu Search', 'siteorigin-corp' ),
 					'description' => esc_html__( 'Display a search icon in the main menu.', 'siteorigin-corp' )
 				),
-
 				'post' => array(
 					'type' => 'checkbox',
 					'label' => esc_html__( 'Post Navigation', 'siteorigin-corp' ),
 					'description' => esc_html__( 'Display the next/previous post navigation.', 'siteorigin-corp' )
 				),
-
 				'scroll_to_top' => array(
 					'type' => 'checkbox',
 					'label' => esc_html__( 'Scroll to Top', 'siteorigin-corp' ),
@@ -94,20 +88,72 @@ function siteorigin_corp_settings_init() {
 			),
 		),		
 
+		// Blog.
+		'blog' => array(
+			'title' => esc_html__( 'Blog', 'siteorigin-corp' ),
+			'fields' => array(
+				'archive_featured_image' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Archive Featured Image', 'siteorigin-corp' ),
+					'description' => esc_html__( 'Display the featured image on the archive and single post pages.', 'siteorigin-corp' )
+				),
+				'post_excerpt_read_more_link' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Post Excerpt Read More Link', 'siteorigin-corp' ),
+					'description' => esc_html__( 'Display the Read More link below the post excerpt.', 'siteorigin-corp' )
+				),
+				'post_featured_image' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Post Featured Image', 'siteorigin-corp' ),
+					'description' => esc_html__( 'Display the featured image on the single post page.', 'siteorigin-corp' )
+				),
+				'post_comment_count' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Post Comment Count', 'siteorigin-corp' ),
+					'description' => esc_html__( 'Display the post comment count on the archive and single post pages.', 'siteorigin-corp' )
+				),				
+				'post_tags' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Post Tags', 'siteorigin-corp' ),
+					'description' => esc_html__( 'Display the post tags on single post pages.', 'siteorigin-corp' )
+				),				
+				'post_author_box' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Post Author Box', 'siteorigin-corp' ),
+					'description' => esc_html__( 'Display the post author biographical info.', 'siteorigin-corp' )
+				),				
+				'related_posts' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Related Posts', 'siteorigin-corp' ),
+					'description' => esc_html__( 'Display related posts on the single post page.', 'siteorigin-corp' )
+				),						
+				'ajax_comments' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Ajax Comments', 'siteorigin-corp' ),
+					'description' => esc_html__( 'Keep the conversation flowing with ajax loading comments.', 'siteorigin-corp' ),
+					'teaser' => true,
+				),
+			),											
+		),	
+
 		// Footer.
 		'footer' => array(
 			'title' => esc_html__( 'Footer', 'siteorigin-corp' ),
 			'fields' => array(
-
 				'text' => array(
 					'type' => 'text',
 					'label' => esc_html__( 'Footer Text', 'siteorigin-corp' ),
 					'description' => esc_html__( "{sitename} and {year} are your site's name and current year.", 'siteorigin-corp' ),
 					'sanitize_callback' => 'wp_kses_post',
 				),
+				'attribution' => array(
+					'type' => 'checkbox',
+					'label' => esc_html__( 'Hide SiteOrigin Attribution', 'siteorigin-north' ),
+					'description' => esc_html__( 'Hide the SiteOrigin link in your footer.', 'siteorigin-north' ),
+					'teaser' => true,
+				),
 			),
-		),		
-
+		),
 	) ) );
 }
 add_action( 'siteorigin_settings_init', 'siteorigin_corp_settings_init' );
@@ -169,8 +215,17 @@ function siteorigin_corp_settings_defaults( $defaults ) {
 	$defaults['navigation_post']						= true;
 	$defaults['navigation_scroll_to_top']				= true;
 
+	// Blog.
+	$defaults['blog_archive_featured_image']			= true;
+	$defaults['blog_post_excerpt_read_more_link']		= true;
+	$defaults['blog_post_featured_image']				= true;
+	$defaults['blog_post_comment_count']				= true;
+	$defaults['blog_post_tags']							= true;
+	$defaults['blog_post_author_box']					= true;
+	$defaults['blog_related_posts']						= true;
+
 	// Footer.
-	$defaults['footer_text']	= esc_html__( '{year} &copy; {sitename}.', 'siteorigin-corp' );
+	$defaults['footer_text']							= esc_html__( '{year} &copy; {sitename}.', 'siteorigin-corp' );
 
 	return $defaults;
 }
