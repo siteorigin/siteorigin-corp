a951e39c86f3c6764613fbb7cb41ce112d7ed47d
Andrew Misplon <amisplon@gmail.com>
2017-11-16 17:13:48 +0200

    Fix single featured image setting

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,7 +17,7 @@
 		<div class="entry-thumbnail">
 			<?php the_post_thumbnail(); ?>
 		</div>
-	<?php elseif ( has_post_thumbnail() && siteorigin_setting( 'blog_archive_featured_image' ) ) : ?>
+	<?php elseif ( ! is_single() && has_post_thumbnail() && siteorigin_setting( 'blog_archive_featured_image' ) ) : ?>
 		<div class="entry-thumbnail">
 			<a href="<?php the_permalink(); ?>">
 				<?php the_post_thumbnail( 'siteorigin-corp-551x364-crop' ); ?>		
