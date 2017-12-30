1953d7e9177c5d6c62a4472da6f03ec52c31b7bd
Andrew Misplon <amisplon@gmail.com>
2017-12-29 18:50:11 -0800

    Added the about page

---
+++ admin/about/page-free.php
@@ -0,0 +1,5 @@
+<h3><?php esc_html_e( 'Forever Free', 'siteorigin-corp' ) ?></h3>
+<p>
+	<?php esc_html_e( "Corp is a completely free WordPress theme.", 'siteorigin-corp' ) ?>
+	<?php esc_html_e( "We'll continue developing and enhancing it for years to come.", 'siteorigin-corp' ) ?>
+</p>
