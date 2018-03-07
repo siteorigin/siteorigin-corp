46a4d06f54fe66dc486b331e7ea13725dafaccf1
Andrew Misplon <amisplon@gmail.com>
2018-03-07 09:06:00 +0200

    Add WooCommerce section to about page

---
+++ admin/about/page-woocommerce.php
@@ -0,0 +1,6 @@
+<h3><?php _e( 'WooCommerce Integration', 'siteorigin-unwind' ) ?></h3>
+<img src="<?php echo get_template_directory_uri() ?>/admin/about/woocommerce.png" class="about-image-right about-image-no-text-below" />
+<p>
+	<?php _e( "Unwind is a full-featured WooCommerce theme.", 'siteorigin-unwind' ) ?>
+	<?php _e( "Use it to build your full online store with a stunning design and features like quick-view.", 'siteorigin-unwind' ) ?>
+</p>
