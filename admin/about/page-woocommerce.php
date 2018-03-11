943ee3cba3586647bb71aae79d1278d109070a5f
Andrew Misplon <amisplon@gmail.com>
2018-03-11 12:09:00 +0200

    Minor WC about page adjustment
    Previously made these changes to Unwind. Facepalm.

--- admin/about/page-woocommerce.php
+++ admin/about/page-woocommerce.php
@@ -1,6 +1,6 @@
-<h3><?php _e( 'WooCommerce Integration', 'siteorigin-corp' ) ?></h3>
-<img src="<?php echo get_template_directory_uri() ?>/admin/about/woocommerce.png" class="about-image-right about-image-no-text-below" />
+<h3><?php esc_html_e( 'WooCommerce Integration', 'siteorigin-corp' ) ?></h3>
+<img src="<?php echo get_template_directory_uri(); ?>/admin/about/woocommerce.png" class="about-image-right about-image-no-text-below" />
 <p>
-	<?php _e( "Corp is a full-featured WooCommerce theme.", 'siteorigin-corp' ) ?>
-	<?php _e( "Use it to build your full online store with a stunning design.", 'siteorigin-corp' ) ?>
+	<?php esc_html_e( "Corp is a full-featured WooCommerce theme.", 'siteorigin-corp' ) ?>
+	<?php esc_html_e( "Use it to build your full online store with a stunning design and features.", 'siteorigin-corp' ) ?>
 </p>
