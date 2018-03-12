7b0d0063ac182c6e64b0cc1188aa873ce4af4d13
Andrew Misplon <amisplon@gmail.com>
2018-03-12 10:11:12 +0200

    Remove duplicate WC js file. Add WC file to build.

--- build-config.js
+++ build-config.js
@@ -54,6 +54,7 @@ module.exports = {
     js: {
         src: [
             'js/**/*.js',
+            'woocommerce/js/**/*.js',
             'inc/settings/js/**/*.js',
             'inc/panels-lite/js/**/*.js',
             '!{node_modules,node_modules/**}',  // Ignore node_modules/ and contents
