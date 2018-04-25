9a0aeb29219f64730cd164361bf004336f3efa3c
Andrew Misplon <amisplon@gmail.com>
2018-04-25 12:45:24 +0200

    Adjust about page WooCommerce blurb

--- admin/about/page-woocommerce.php
+++ admin/about/page-woocommerce.php
@@ -2,5 +2,5 @@
 <img src="<?php echo get_template_directory_uri(); ?>/admin/about/woocommerce.png" class="about-image-right about-image-no-text-below" />
 <p>
 	<?php esc_html_e( "Corp is a full-featured WooCommerce theme.", 'siteorigin-corp' ); ?>
-	<?php esc_html_e( "Use it to build your full online store with a stunning design and features.", 'siteorigin-corp' ); ?>
+	<?php esc_html_e( "Use it to build your complete online store with a stunning design and features.", 'siteorigin-corp' ); ?>
 </p>
