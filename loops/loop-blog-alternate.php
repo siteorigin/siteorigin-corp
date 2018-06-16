2f07cfd7ec9c554f0c9a4f0fa29943d7b4cd666f
Andrew Misplon <amisplon@gmail.com>
2018-06-16 20:38:56 +0200

    Use the main template parts for the alternate loop

--- loops/loop-blog-alternate.php
+++ loops/loop-blog-alternate.php
@@ -27,8 +27,8 @@ if ( have_posts() ) :
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'alternate' );
-
+				get_template_part( 'template-parts/content', get_post_format() );
+				
 			endwhile; ?>
 
 	</div><?php
