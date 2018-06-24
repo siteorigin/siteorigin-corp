d3e66e4b88b264099317687df2de9fbfc8574af4
SiteOrigin Support <support@siteorigin.com>
2018-06-24 13:58:59 -0700

    Added back missing functions

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -249,29 +249,88 @@ function siteorigin_corp_excerpt() {
 	} else {
 		$read_more_text = '';
 	}
-	$ellipsis = '...';
-	$length = siteorigin_setting( 'blog_excerpt_length' );
+
+	if ( is_search() ) {
+		$ellipsis = '';
+		$length = 40;
+	} else {
+		$ellipsis = '...';
+		$length = siteorigin_setting( 'blog_excerpt_length' );
+	}
+	
 	$excerpt = explode( ' ', get_the_excerpt(), $length );
 
 	if ( $length ) {
-
 		if ( count( $excerpt ) >= $length ) {
 			array_pop( $excerpt );
 			$excerpt = '<p>' . implode( " ", $excerpt ) . $ellipsis . '</p>' . $read_more_text;
 		} else {
 			$excerpt = '<p>' . implode( " ", $excerpt ) . $ellipsis . '</p>';
 		}
-
 	} else {
-		
 		$excerpt = get_the_excerpt();
-
 	}
 
 	$excerpt = preg_replace( '`\[[^\]]*\]`','', $excerpt );
 
 	echo $excerpt;
 
+}
+endif;
+
+if ( ! function_exists( 'siteorigin_corp_entry_thumbnail_meta' ) ) :
+/**
+ * Print HTML with meta information for the sticky status, current post-date/time, author, comment count and post categories.
+ */
+function siteorigin_corp_entry_thumbnail_meta() {
+	echo '<div class="thumbnail-meta">';
+	if ( has_category() ) {
+		echo get_the_category_list();
+	}
+	echo '</div>';
+}
+endif;
+
+if ( ! function_exists( 'siteorigin_corp_offset_post_meta' ) ) :
+/**
+ * Print HTML with meta information for the post author, categories and comment count for the offset blog layout.
+ */
+function siteorigin_corp_offset_post_meta() {
+	$num_comments = get_comments_number();
+	if ( comments_open() ) {
+		if ( $num_comments == 0 ) {
+			$comments = esc_html__( 'Post a Comment', 'siteorigin-corp' );
+		} elseif ( $num_comments > 1 ) {
+			$comments = $num_comments . esc_html__( ' Comments', 'siteorigin-corp' );
+		} else {
+			$comments = esc_html__( '1 Comment', 'siteorigin-corp' );
+		}
+	} else {
+		$comments = NULL;
+	} ?>
+	<div class="entry-author-avatar">
+		<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+			<?php echo get_avatar( get_the_author_meta( 'ID' ), 70 ); ?>
+		</a>
+	</div>
+	<div class="entry-author-link">
+		<span class="meta-text"><?php esc_html_e( 'Written by', 'siteorigin-corp' ); ?></span>
+		<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+			<?php echo get_the_author(); ?>
+		</a>
+	</div>
+	<?php if ( siteorigin_setting( 'blog_post_categories' ) ) : ?>
+		<div class="entry-categories">
+			<span class="meta-text"><?php esc_html_e( 'Posted in', 'siteorigin-corp' ); ?></span>
+			<?php the_category( ', ', '', '' ); ?>
+		</div>
+	<?php endif; ?>
+	<?php if ( $comments && siteorigin_setting( 'blog_post_comment_count' ) ) : ?>
+		<div class="entry-comments">
+			<span class="meta-text"><?php esc_html_e( 'Comments', 'siteorigin-corp' ); ?></span>
+			<a href="<?php get_comments_link(); ?>"><?php echo $comments; ?></a>
+		</div>
+	<?php endif;
 }
 endif;
 
