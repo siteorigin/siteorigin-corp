02a1e2f051df48043040f30d8d06adfd9e65232c
Andrew Misplon <amisplon@gmail.com>
2018-07-13 18:06:22 +0200

    Removed post loop h1 tags

--- loops/loop-blog-masonry.php
+++ loops/loop-blog-masonry.php
@@ -10,15 +10,7 @@ wp_enqueue_script( 'jquery-masonry' );
 ?>
 
 <?php
-if ( have_posts() ) :
-
-	if ( is_home() && ! is_front_page() ) : ?>
-		<header>
-			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-		</header>
-
-	<?php
-	endif; ?>
+if ( have_posts() ) : ?>
 
 	<div class="blog-layout-masonry">
 
