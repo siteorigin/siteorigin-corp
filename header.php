c321d1958ad3276a1e631980af8eb33fd29fb748
Andrew Misplon <amisplon@gmail.com>
2018-08-21 10:20:39 +0200

    Check if WooCommerce is active for mini-cart

--- header.php
+++ header.php
@@ -49,7 +49,7 @@
 
 					<?php if ( siteorigin_setting( 'navigation_header_menu' ) ) wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 
-					<?php if ( siteorigin_setting( 'woocommerce_mini_cart' ) && ! $mega_menu_active ) siteorigin_corp_woocommerce_mini_cart(); ?>	
+					<?php if ( function_exists( 'is_woocommerce' ) && siteorigin_setting( 'woocommerce_mini_cart' ) && ! $mega_menu_active ) siteorigin_corp_woocommerce_mini_cart(); ?>	
 
 					<?php if ( siteorigin_setting( 'navigation_menu_search' ) ) : ?>
 		            	<button id="search-button" class="search-toggle">
