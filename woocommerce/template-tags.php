09d455c83772c27fa318e494b9ccc7f028e44dc7
Andrew Misplon <amisplon@gmail.com>
2018-08-25 19:33:21 +0200

    WC archive button hover styles

--- woocommerce/template-tags.php
+++ woocommerce/template-tags.php
@@ -77,6 +77,9 @@ function siteorigin_corp_woocommerce_archive_product_image() { ?>
 		<?php woocommerce_template_loop_product_link_open(); ?>
 		<?php woocommerce_template_loop_product_thumbnail(); ?>
 		<?php woocommerce_template_loop_product_link_close(); ?>
+		<?php if ( siteorigin_setting( 'woocommerce_quick_view' ) || siteorigin_setting( 'woocommerce_add_to_cart' ) ) : ?>
+		<div class="corp-wc-overlay"></div>
+		<?php endif; ?>
 		<?php if ( siteorigin_setting( 'woocommerce_quick_view' ) ) {
 			siteorigin_corp_woocommerce_quick_view_button();
 		} ?>		
@@ -147,8 +150,8 @@ if ( ! function_exists( 'siteorigin_corp_woocommerce_quick_view_button' ) ) :
  * Quick view button for the products in loop
  */
 function siteorigin_corp_woocommerce_quick_view_button() {
-	global $product;
-	echo '<a href="#" id="product-id-' . $product->get_id() . '" class="button product-quick-view-button" data-product-id="' . $product->get_id() . '">' . esc_html__( 'Quick View', 'siteorigin-corp') . '</a>';
+	global $product;	
+	echo '<a href="#" id="product-id-' . $product->get_id() . '" class="product-quick-view-button" data-product-id="' . $product->get_id() . '"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="26" height="28" viewBox="0 0 26 28"><path d="M18 13c0-3.859-3.141-7-7-7s-7 3.141-7 7 3.141 7 7 7 7-3.141 7-7zM26 26c0 1.094-0.906 2-2 2-0.531 0-1.047-0.219-1.406-0.594l-5.359-5.344c-1.828 1.266-4.016 1.937-6.234 1.937-6.078 0-11-4.922-11-11s4.922-11 11-11 11 4.922 11 11c0 2.219-0.672 4.406-1.937 6.234l5.359 5.359c0.359 0.359 0.578 0.875 0.578 1.406z"></path></svg></a>';
 }
 endif;
 
