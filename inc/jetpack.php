76f743a663f337d97e87cfd4703eb146e178ed1b
Andrew Misplon <amisplon@gmail.com>
2018-09-27 12:25:18 +0200

    Spacing fixes

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -110,11 +110,11 @@ endif;
  * Remove sharing buttons from their default locations
  */
  function siteorigin_corp_remove_share() {
-    remove_filter( 'the_content', 'sharing_display', 19 );
-    remove_filter( 'the_excerpt', 'sharing_display', 19 );
-    if ( class_exists( 'Jetpack_Likes' ) ) {
-        remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
-    }
+	remove_filter( 'the_content', 'sharing_display', 19 );
+	remove_filter( 'the_excerpt', 'sharing_display', 19 );
+	if ( class_exists( 'Jetpack_Likes' ) ) {
+		remove_filter( 'the_content', array( Jetpack_Likes::init(), 'post_likes' ), 30, 1 );
+	}
 }
 add_action( 'loop_start', 'siteorigin_corp_remove_share' );
 
