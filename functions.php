ff1f12aa2a9a2cd5bf4dd9f68bb9b561e3b7412b
Andrew Misplon <amisplon@gmail.com>
2018-09-27 12:24:08 +0200

    Don’t enqueue FitVids.js if it’s a Gutenberg page

--- functions.php
+++ functions.php
@@ -167,7 +167,7 @@ function siteorigin_corp_scripts() {
 	// FitVids.
 	wp_register_script( 'jquery-fitvids', get_template_directory_uri() . '/js/jquery.fitvids' . SITEORIGIN_THEME_JS_PREFIX . '.js', array( 'jquery' ), '1.1', true );
 
-	if ( ! class_exists( 'Jetpack' ) ) {
+	if ( ! class_exists( 'Jetpack' ) && ! ( function_exists( 'has_blocks' ) && has_blocks() ) ) {
 		wp_enqueue_script( 'jquery-fitvids' );
 	}
 
