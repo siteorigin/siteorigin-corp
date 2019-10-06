73f6c03c4061744a02e1bfb9c301e59ef6a1b651
Andrew Misplon <amisplon@gmail.com>
2019-10-06 18:45:00 +0200

    Updated WooCommerce settings CSS

--- inc/settings.php
+++ inc/settings.php
@@ -1198,14 +1198,22 @@ function siteorigin_corp_wc_settings_custom_css( $css ) {
 	.woocommerce a .star-rating {
 	color: ${typography_accent};
 	}
+	.woocommerce .products .product .loop-product-thumbnail .added_to_cart {
+	background: ${typography_accent};
+	}
+	.woocommerce .products .product .loop-product-thumbnail .added_to_cart:hover {
+	background: .rgba( ${typography_accent}, .8);
+	}
 	.woocommerce .products .product .woocommerce-loop-product__title:hover,.woocommerce .products .product .woocommerce-loop-category__title:hover {
 	color: ${typography_accent};
 	}
-	.woocommerce .products .product .added_to_cart {
-	background: ${typography_accent};
+	.woocommerce .products .product > .button,.woocommerce .products .product .panel-grid-cell .button {
+	border-color: ${typography_heading};
+	color: ${typography_heading};
 	}
-	.woocommerce .products .product .added_to_cart:hover {
-	background: .rgba( ${typography_accent}, .8);
+	.woocommerce .products .product > .button:hover,.woocommerce .products .product .panel-grid-cell .button:hover {
+	border-color: ${typography_accent};
+	color: ${typography_accent};
 	}
 	.woocommerce .price {
 	color: ${typography_text};
@@ -1213,38 +1221,38 @@ function siteorigin_corp_wc_settings_custom_css( $css ) {
 	.woocommerce .price ins {
 	color: ${typography_accent};
 	}
-	.woocommerce .product .summary .woocommerce-review-link,.woocommerce .product .product-info-wrapper .woocommerce-review-link {
+	.woocommerce .product .woocommerce-review-link {
 	color: ${typography_secondary_text};
 	}
-	.woocommerce .product .summary .woocommerce-review-link:hover,.woocommerce .product .product-info-wrapper .woocommerce-review-link:hover {
+	.woocommerce .product .woocommerce-review-link:hover {
 	color: ${typography_accent};
 	}
-	.woocommerce .product .summary .variations .label label,.woocommerce .product .product-info-wrapper .variations .label label {
+	.woocommerce .product .variations .label label {
 	color: ${typography_heading};
 	}
-	.woocommerce .product .summary .variations .reset_variations,.woocommerce .product .product-info-wrapper .variations .reset_variations {
+	.woocommerce .product .variations .reset_variations {
 	color: ${typography_text};
 	}
-	.woocommerce .product .summary .variations .reset_variations:hover,.woocommerce .product .product-info-wrapper .variations .reset_variations:hover {
+	.woocommerce .product .variations .reset_variations:hover {
 	color: ${typography_accent};
 	}
-	.woocommerce .product .summary .woocommerce-grouped-product-list td a,.woocommerce .product .product-info-wrapper .woocommerce-grouped-product-list td a {
+	.woocommerce .product .woocommerce-grouped-product-list td a {
 	color: ${typography_text};
 	}
-	.woocommerce .product .summary .woocommerce-grouped-product-list td a:hover,.woocommerce .product .product-info-wrapper .woocommerce-grouped-product-list td a:hover {
+	.woocommerce .product .woocommerce-grouped-product-list td a:hover {
 	color: ${typography_accent};
 	}
-	.woocommerce .product .summary .stock,.woocommerce .product .product-info-wrapper .stock {
+	.woocommerce .product .stock {
 	color: ${typography_accent};
 	}
-	.woocommerce .product .summary .product_meta,.woocommerce .product .product-info-wrapper .product_meta {
+	.woocommerce .product .product_meta {
 	border-top: 1px solid ${typography_border};
 	color: ${typography_text};
 	}
-	.woocommerce .product .summary .product_meta a,.woocommerce .product .product-info-wrapper .product_meta a {
+	.woocommerce .product .product_meta a {
 	color: ${typography_heading};
 	}
-	.woocommerce .product .summary .product_meta a:hover,.woocommerce .product .product-info-wrapper .product_meta a:hover {
+	.woocommerce .product .product_meta a:hover {
 	color: ${typography_accent};
 	}
 	.woocommerce .product .woocommerce-tabs h2 {
