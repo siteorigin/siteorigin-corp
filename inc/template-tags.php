279cad0a4dbd3ee2ab8ce3922d56482f11ec02dd
Andrew Misplon <amisplon@gmail.com>
2019-10-10 18:02:32 +0200

    Improved Jetpack Related posts conditional

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -341,7 +341,7 @@ if ( ! function_exists( 'siteorigin_corp_related_posts' ) ) :
 function siteorigin_corp_related_posts( $post_id ) {
 	if ( function_exists( 'related_posts' ) ) { // Check for YARPP plugin (https://wordpress.org/plugins/yet-another-related-posts-plugin/).
 		related_posts();
-	} elseif ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'related-posts' ) ) {
+	} elseif ( class_exists( 'Jetpack' ) && class_exists( 'Jetpack_RelatedPosts' ) ) {
 		echo do_shortcode( '[jetpack-related-posts]' );
 	} else { // The fallback loop.
 		$categories = get_the_category( $post_id );
@@ -384,7 +384,7 @@ if ( ! function_exists( 'siteorigin_corp_related_projects' ) ) :
  * Displays related posts in single projects.
  */
 function siteorigin_corp_related_projects( $post_id ) {
-	if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'related-posts' ) ) {
+	if ( class_exists( 'Jetpack' ) && class_exists( 'Jetpack_RelatedPosts' ) ) {
 		echo do_shortcode( '[jetpack-related-posts]' );
 	} else { // The fallback loop.
 		$categories = get_the_terms( $post_id, 'jetpack-portfolio-type' );
