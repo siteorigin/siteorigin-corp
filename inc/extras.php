90c0b3e37f0e302c48655d8a426ab5546dc05006
Andrew Misplon <amisplon@gmail.com>
2020-04-16 15:30:01 +0200

    Minor formatting fix

--- inc/extras.php
+++ inc/extras.php
@@ -52,7 +52,7 @@ function siteorigin_corp_body_classes( $classes ) {
 
 	if ( siteorigin_setting( 'sidebar_position' ) == 'left' && ! is_404() && ! ( function_exists( 'is_woocommerce' ) && is_woocommerce() ) ) {
 		$classes[] = 'sidebar-left';
-	}		
+	}
 
 	// WooCommerce top bar.
 	if ( class_exists( 'Woocommerce' ) && ! is_store_notice_showing() ) {
@@ -92,7 +92,7 @@ add_action( 'wp_head', 'siteorigin_corp_pingback_header' );
 if ( ! function_exists( 'siteorigin_corp_unset_current_menu_class' ) ) :
 /**
  * Unset the current menu class.
- */	
+ */
 function siteorigin_corp_unset_current_menu_class( $classes ) {
 	$disallowed_class_names = array(
 		'current-menu-item',
