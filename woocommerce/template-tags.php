6edb4f77b32ee203405d9f37b9e65664bf69612b
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-08-26 16:57:34 +1000

    Quick View: Add Gallery
    Duplicate of https://github.com/siteorigin/siteorigin-unwind/blob/1.6.6/woocommerce/template-tags.php#L110-L146

--- woocommerce/template-tags.php
+++ woocommerce/template-tags.php
@@ -171,7 +171,41 @@ if ( ! function_exists( 'siteorigin_corp_woocommerce_quick_view_image' ) ) :
  * Displays image in the product quick view.
  */
 function siteorigin_corp_woocommerce_quick_view_image() {
-	echo woocommerce_get_product_thumbnail( 'shop_single' );
+	global $product;
+	$gallery = $product->get_gallery_image_ids();
+
+	if ( empty( $gallery ) && ! has_post_thumbnail() ) return;
+
+	if ( empty( $gallery ) ) {
+		echo woocommerce_get_product_thumbnail( 'shop_single' );
+	} else {
+		?>
+		<div class="product-images-slider flexslider">
+			<ul class="slides">
+				<?php if ( has_post_thumbnail() ) {
+					$image_title = esc_attr( get_the_title( get_post_thumbnail_id() ) );
+					$image_element = get_the_post_thumbnail( $product->get_id(), apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array( 'title' => $image_title, 'alt' => $image_title ) );
+					echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<li class="slide product-featured-image">%s</li>', $image_element ), $product->get_id() );
+				} ?>
+
+				<?php if ( $gallery ) {
+					foreach ( $gallery as $image ) {
+						$image_link = wp_get_attachment_url( $image );
+						$image_title = esc_attr( get_the_title( $image ) );
+						?>
+
+						<li class="slide product-gallery-image">
+							<img src="<?php echo $image_link; ?>" alt="<?php echo $image_title ?>" title="<?php echo $image_title ?>" />
+						</li>
+
+						<?php
+					}
+				} ?>
+
+			</ul>
+		</div>
+	<?php
+	}
 }
 endif;
 
