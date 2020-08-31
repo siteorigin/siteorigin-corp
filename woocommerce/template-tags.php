9e6fb3c0bb9a0e4c8d855568ff2f9bba0355957e
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-08-31 12:10:38 +1000

    Quick View: Use Corp Custom Flexslider Nav

--- woocommerce/template-tags.php
+++ woocommerce/template-tags.php
@@ -203,6 +203,15 @@ function siteorigin_corp_woocommerce_quick_view_image() {
 				} ?>
 
 			</ul>
+
+			<ul class="flex-direction-nav">
+				<li class="flex-nav-prev">
+					<a class="flex-prev" href="#"><?php siteorigin_corp_display_icon( 'left-arrow' ); ?></a>
+				</li>
+				<li class="flex-nav-next">
+					<a class="flex-next" href="#"><?php siteorigin_corp_display_icon( 'right-arrow' ); ?></a>
+				</li>
+			</ul>
 		</div>
 	<?php
 	}
