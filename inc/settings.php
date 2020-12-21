a9acfaa1cc5d6c0b3c01e805014715316a8ba452
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2020-12-21 21:16:34 +1000

    Use Settigns Lazy Load Exclude Logo

--- inc/settings.php
+++ inc/settings.php
@@ -1820,3 +1820,6 @@ function siteorigin_corp_about_page_sections( $about ) {
 	return $about;
 }
 add_filter( 'siteorigin_about_page', 'siteorigin_corp_about_page_sections' );
+
+// Enable Lazy Load Exclude Logo Settings Extra.
+add_filter( 'siteorigin_settings_lazy_load_exclude_logo', '__return_true' );
