264cfb12a0e9e7b8f5ff8f71d3fa1507e9f149cb
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-04-14 07:02:32 +1200

    Resolve Missing Text Domain
    ` WARNING: Found a translation function that is missing a text-domain in the file inc/settings.php. Function __, with the arguments 'siteorigin-corp'.
    `

--- inc/settings.php
+++ inc/settings.php
@@ -37,13 +37,10 @@ function siteorigin_corp_settings_init() {
 					'label'       => esc_html__( 'Retina Logo', 'siteorigin-corp' ),
 					'description' => esc_html__( 'A logo for use on high pixel density displays. Must be used in addition to a regular logo added in the Site Identity section and be exactly double the size.', 'siteorigin-corp' ),
 					'teaser' => array(
-						'text' => __(
-							sprintf(
-								'Enhance your SiteOrigin theme logo functionality with the %sLogo Booster Addon%s. Add an alternative logo on any page; upload a sticky logo to display on scroll.',
-								'<a href="https://siteorigin.com/downloads/premium/?featured_addon=theme/logo-booster" target="_blank" rel="noopener noreferrer">',
-								'</a>'
-							),
-							'siteorigin-corp'
+						'text' => sprintf(
+							__( 'Enhance your SiteOrigin theme logo functionality with the %sLogo Booster Addon%s. Add an alternative logo on any page; upload a sticky logo to display on scroll.', 'siteorigin-corp' ),
+							'<a href="https://siteorigin.com/downloads/premium/?featured_addon=theme/logo-booster" target="_blank" rel="noopener noreferrer">',
+							'</a>'
 						),
 					),
 				),
